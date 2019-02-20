cc9df2f7ec83cfe05ab48f31392a33d23b4cb981
skadefar <tobias.j.hansen@gmail.com>
2019-02-20 10:46:02 +0100

    This right here is my.. Pretty Boy Swag Windows have been made and some buttons work

---
+++ Kvalitet1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kvalitet1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpretOrdrebutton_Click(object sender, RoutedEventArgs e)
+        {
+            OpretOrdreWindow ordre = new OpretOrdreWindow();
+            ordre.Show();
+            this.Close();
+
+        }
+
+        private void OpretKundebutton_Click(object sender, RoutedEventArgs e)
+        {
+            OpretKundeWindow opretkunde = new OpretKundeWindow();
+            opretkunde.Show();
+            this.Close();
+
+        }
+
+        private void FindKundebutton_Click(object sender, RoutedEventArgs e)
+        {
+            FindKundeWindow findkunde = new FindKundeWindow();
+            findkunde.Show();
+            this.Close();
+
+        }
+    }
+}
