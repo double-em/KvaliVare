35f95236b88360955732e518ec3380ee2f847140
ande714b <43536499+ande714b@users.noreply.github.com>
2019-02-28 12:37:03 +0100

    Test

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8fd6e054-cff1-4e30-9dd6-6d46639df7c8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KvalitetLibrary.App;
+using KvalitetLibrary.Domain;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+     
+        [TestMethod]
+        public void TestCreateCustomer()
+        {
+            CustomerRepository c = new CustomerRepository();
+            Customer cust = c.CreateCustomer(1, "test", "testaddress", "1234", "testtown", "12344567");
+            c.AddCustomer(cust);
+            Assert.AreEqual(cust,c.GetCustomer("1"));
+        }
+    }
+}
